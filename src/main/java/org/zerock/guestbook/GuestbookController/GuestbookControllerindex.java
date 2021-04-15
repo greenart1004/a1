16801b39259e904792f872ed18505cce58b786e8
greenart <tkfkd7887@naver.com>
2021-04-15 22:16:44 +0900

    112

--- src/main/java/org/zerock/guestbook/GuestbookController/GuestbookControllerindex.java
+++ src/main/java/org/zerock/guestbook/GuestbookController/GuestbookControllerindex.java
@@ -1,24 +1,24 @@
-package org.zerock.guestbook.GuestbookController;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.guestbook.service.GuestbookService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-
-@RequestMapping("")
-@Controller
-@Log4j2
-@RequiredArgsConstructor
-class GuestbookControllerindex {
-	
-	@GetMapping("/index")
-	public String list() {
-	log.info("list.............................");
-
-		return "/guestbook/index"; // templates 바로 아래에 있는 주소
-	}
-
-}
+//package org.zerock.guestbook.GuestbookController;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.zerock.guestbook.service.GuestbookService;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//
+//@RequestMapping("")
+//@Controller
+//@Log4j2
+//@RequiredArgsConstructor
+//class GuestbookControllerindex {
+//	
+//	@GetMapping("/index")
+//	public String list() {
+//	log.info("list.............................");
+//
+//		return "/guestbook/index"; // templates 바로 아래에 있는 주소
+//	}
+//
+//}
